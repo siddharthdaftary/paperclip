78b2599ebaaedceb77662268e782b5f830bb4952
anonymous anonymous <siddharth.daftary@kellogg.northwestern.edu>
2021-12-08 00:40:09 +0000

    Add JWT Concern.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  # Direct associations
+  
+  include JwtToken
+# Direct associations
 
   has_many   :recieved_follow_requests,
              :class_name => "FollowRequest",

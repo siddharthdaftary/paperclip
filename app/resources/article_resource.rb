0755f8a7fb56de562e3d3574a6fcc9782e2abc3a
anonymous anonymous <siddharth.daftary@kellogg.northwestern.edu>
2021-12-08 00:39:51 +0000

    Generate graphiti resources for articles

---
+++ app/resources/article_resource.rb
@@ -0,0 +1,16 @@
+class ArticleResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :link, :string
+  attribute :uploader_review, :string
+  attribute :uploader_id, :integer
+  attribute :publication, :string
+  attribute :estimated_reading_time, :string
+  attribute :tag_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end

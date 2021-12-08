30862dc5f05e2238a59e32af8c28d4b6be1af95a
anonymous anonymous <siddharth.daftary@kellogg.northwestern.edu>
2021-12-08 00:38:36 +0000

    Direct association between articles and comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
   # Direct associations
 
+  has_many   :comments,
+             :dependent => :destroy
+
   has_many   :favorites,
              :dependent => :destroy
 
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Article, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:comments) }
+
     it { should have_many(:favorites) }
 
     it { should belong_to(:uploader) }
